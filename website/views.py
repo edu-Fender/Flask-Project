bc8c01c6f251a3820577c8b3d45a6cfcdc930a22
Eduardo Miguel <aneweduardo@hotmail.com>
2021-05-24 12:35:26 -0300

    Commit final, projeto finalizado.

--- website/views.py
+++ website/views.py
@@ -1,3 +1,7 @@
+## -- IMPORTANTE! --
+## Quando este script é acionado, uma linha em branco é criada no final do arquivo "data.clientes.csv" para evitar que o Pandas insira o DataFrame em uma linha já populada.
+## Certifique-se de deixar uma linha em branco ao modificar o arquivo CSV durante a execução do programa.
+
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 import json
 import requests
@@ -6,7 +10,10 @@ import numpy as np
 import re
 
 views = Blueprint('views', __name__)
-cliente = {}  ### Váriavel global que armazena toda informação relativa ao cliente
+
+with open('data/clientes.csv', 'a') as f:  # Cria uma linha em branco no final do CSV para evitar que o Pandas insira o dataframe em uma linha já populada
+    f.write('\n')
+
 
 ## -- PÁGINA INICIAL --
 @views.route('/')
@@ -27,42 +34,46 @@ def clientes():
     df = df.replace(np.nan, '', regex=True)
     return render_template('clientes.html', df=df, titles=df.columns.values)
 
+
 ## -- CADASTRO --
-@views.route('/cadastro', methods=['GET', 'POST'])  ### Adicionada permissão para POST Requests
+@views.route('/cadastro', methods=['GET', 'POST'])  # Adicionada permissão para POST Requests
 def cadastro():
     """
     Função para cadastro de novos clientes. Deverá pegar as informações do forms e salvar numa nova linha no csv.
     Necessário também salvar as informações de endereço provindas da API de CEP
     """
+
     ## TODO pegar informações do forms
     if request.method == 'POST':
         cliente = {'nome': request.form['nome'], 'sobrenome' : request.form['sobrenome'], 'email' : request.form['email']}
         
     ## TODO buscar informações de endereço da API do ViaCEP (https://viacep.com.br/)
         cep = request.form['cep']
-        check = re.search("^(\d{8})$", cep)  ### Pequeno tratamento de erro para garantir o envio correto do CEP para a API
 
+        check = re.search("^(\d{8})$", cep)  # Pequeno tratamento de erro em ReGex para garantir o envio correto do CEP para a API
         if check == None:
             flash("CEP inválido. O CEP deve conter 8 NÚMEROS.", "error")
             return render_template('cadastro.html')
 
         viacep = requests.get(f'http://viacep.com.br/ws/{cep}/json/', cep)
-        viacep = viacep.json()  ### Transforma o objeto em JSON (dicionário)
+        viacep = viacep.json()  # Transforma o objeto de retorno da API em JSON
 
-        if ('erro' in viacep):  ### Quando um CEP é inserido corretamente porém não é encontrado pela API, o retorno é um JSON: "{'erro' = True}"
+        if ('erro' in viacep):  # Quando um CEP é inserido corretamente, porém não é encontrado pela API, o retorno da API é um JSON: {'erro' = True}
             flash("CEP não encontrado, por favor tente novamente.", "error")
             return render_template('cadastro.html')
         
-        cliente.update(viacep)  ### Adiciona o dicionário "cep" no final do dicionário "cliente"
+        cliente.update(viacep)  # Concatena o dicionário "viacep" ao dicionário "cliente"
+
 
     ## TODO criar nova linha no arquivo csv
         df = pd.DataFrame([cliente])
-        df.to_csv('data/clientes.csv', mode='a', header=False, index=False, sep=';')  ### Cria nova linha no cliente no CSV
+        df.to_csv('data/clientes.csv', mode='a', header=False, index=False, sep=';')  # Insere o DataFrame "cliente" ao CSV
 
         flash("Usuário cadastrado com sucesso!")
-        return redirect(url_for('.home'))
+        return redirect(url_for('.home'))  # Por causa do Blueprint, argmuento do URL_FOR precisa de ponto "." para funcionar
 
     return render_template('cadastro.html')
+
 
 ## -- CONSULTA CEP --
 @views.route('/consulta-cep', methods=['GET', 'POST'])
@@ -72,17 +83,16 @@ def consulta_cep():
     if request.method == 'POST':
         cep = request.form['cep']
 
-        check = re.search("^(\d{8})$", cep)  ### Pequeno tratamento de exceção para garantir o envio correto do CEP para a API
-
+        check = re.search("^(\d{8})$", cep)
         if check == None:
             flash("CEP inválido. O CEP deve conter 8 NÚMEROS.", "error")
             return render_template('consulta_cep.html')
 
     ## TODO buscar informações de endereço da API do ViaCEP (https://viacep.com.br/)
         viacep = requests.get(f'http://viacep.com.br/ws/{cep}/json/', cep)
-        viacep = viacep.json()  ### Transforma o objeto em JSON (dicionário)
+        viacep = viacep.json()
 
-        if ('erro' in viacep):  ### Quando um CEP é inserido corretamente porém não é encontrado plea API, o retorno é um JSON: "{'erro' = True}"
+        if ('erro' in viacep):
             flash("CEP não encontrado, por favor tente novamente.", "error")
             return render_template('consulta_cep.html')
           
